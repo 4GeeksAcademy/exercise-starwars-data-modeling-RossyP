ea7aeff468294f92c8f801cd5246e23de3a91566
Rossy Jasmin Palacin Nuñez <119419018+ROZZYNUEZ14@users.noreply.github.com>
2024-08-26 23:31:03 +0000

    Se modifica la estructura del modelo

--- src/models.py
+++ src/models.py
@@ -7,55 +7,55 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Favorito(Base):
-    __tablename__= "favorito"
-    id = Column(Integer, primary_key=True)
-    name = Column(String(50), nullable=False)
-    usuario_id = Column(Integer, ForeignKey('usuario.id'), nullable=False)
-    personaje_id = Column(Integer, ForeignKey("personaje.id"), nullable=False)
-    planeta_id = Column(Integer, ForeignKey("planeta.id"), nullable=False)
-    especie_id = Column(Integer, ForeignKey("especie.id"), nullable=False)
-    usuarios = relationship("Usuario", backref="favorito", lazy=True)
-    personajes = relationship('Personaje', backref='favorito', lazy=True)
-    planetas = relationship("Planeta", backref="favorito", lazy=True)
-    especies = relationship("Especie", backref="favorito", lazy=True)
+
+
 
 class Usuario(Base):
     __tablename__ = "usuario"
     id = Column(Integer, primary_key=True)
     name = Column(String(50), nullable=False)
     email = Column(String(120), nullable=False)
-    # favorito_id = Column(Integer, ForeignKey('favorito.id'), nullable=False)
     favoritos = relationship("Favorito", backref="usuario", lazy=True)
+
 
 class Personaje(Base):
     __tablename__ = "personaje"
     id = Column(Integer, primary_key=True)
     name = Column(String(50), nullable=False)
-    planetas = relationship("Planeta", backref="planeta")
-    especies = relationship("Especie", backref="especie")
-    especie_id = Column(Integer, ForeignKey('especie.id'))
-    planeta_id = Column(Integer, ForeignKey('planeta.id'))
-    favorito_id = Column(Integer, ForeignKey('favorito.id'), nullable=False)
-
-
+    especie_id = Column(Integer, ForeignKey("especie.id"), nullable=False)
+    planeta_id = Column(Integer, ForeignKey("planeta.id"), nullable=False)
+    favoritos = relationship("Favorito", backref="personaje", lazy=True)
 
 class Planeta(Base):
     __tablename__="planeta"
     id = Column(Integer, primary_key=True)
     name = Column(String(50), nullable=False)
     clima = Column(String(100), nullable=False)
-    favorito_id = Column(Integer, ForeignKey('favorito.id'), nullable=False)
-    # personaje_id = Column(Integer, ForeignKey('personaje.id'), nullable=False)
+    personajes = relationship("Personaje", backref="planeta", lazy=True)
+    favoritos = relationship("Favorito", backref="planeta", lazy=True)
 
 class Especie(Base):
     __tablename__="especie"
     id = Column(Integer, primary_key=True)
     name = Column(String(50), nullable=False)
-    # planeta = Column(String(100), nullable=False)
-    favorito_id = Column(Integer, ForeignKey('favorito.id'), nullable=False)
-    personaje_id = Column(Integer, ForeignKey('personaje.id'), nullable=False)
-    planeta = relationship("Planeta", backref="especies", lazy=True)
+    planeta_id = Column(Integer, ForeignKey("planeta.id"), nullable=False)
+    personajes = relationship("Personaje", backref="especie", lazy=True)
+    favoritos = relationship("Favorito", backref="especie", lazy=True)
+
+
+
+class Favorito(Base):
+    __tablename__= "favorito"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    usuario_id = Column(Integer, ForeignKey("usuario.id"), nullable = False)
+    planeta_id = Column(Integer, ForeignKey("planeta.id"), nullable = False)
+    especie_id = Column(Integer, ForeignKey("especie.id"), nullable = False)
+    personaje_id = Column(Integer, ForeignKey("personaje.id"), nullable = False)
+    # usuarios = relationship("Usuario", backref="usuario", lazy=True)
+    # personajes = relationship('Personaje', backref='personaje', lazy=True)
+    # planetas = relationship("Planeta", backref="planeta", lazy=True)
+    # especies = relationship("Especie", backref="especie", lazy=True)
 
 # -----
 # class Person(Base):
